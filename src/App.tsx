d750bda9e27898e0df754d364678d9520cd27d66
Erick Namukolo <erickmndev@gmail.com>
2024-01-14 18:01:38 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Category from './models/category';
 
-function App() {
+const App = () => {
+  const categories: Category[] = [
+    { id: 1, title: 'Hats' },
+    { id: 2, title: 'Jackets' },
+    { id: 3, title: 'Sneakers' },
+    { id: 4, title: "Women's" },
+    { id: 5, title: "Men's" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='categories-container'>
+      {categories.map((category) => {
+        return (
+          <div className='category-container'>
+            {/* <img /> */}
+            <div className='category-body-container'>
+              <h2>{category.title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
