e352c84c33236444aaa6f3d8927a795cc677d4ea
Erick Namukolo <erickmndev@gmail.com>
2024-01-17 22:36:29 +0200

    added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import Home from './routes/home/home_route';
 import { Route, Routes } from 'react-router-dom';
 import NavigationBar from './components/navigation_bar/navigation';
+import SignIn from './routes/sign-in/sign-in';
 
 const Shop = () => {
   return <div>Hey im a shop</div>;
-};
-
-const Store = () => {
-  return <div>Hey im a store</div>;
 };
 
 const App = () => {
@@ -17,7 +14,7 @@ const App = () => {
       <Route path='/' element={<NavigationBar />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='/shop/store' element={<Store />} />
+        <Route path='/sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/sign-in/sign-in.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignIn: React.FC = (props) => {
+  return <h1>Sign In page</h1>;
+};
+
+export default SignIn;
