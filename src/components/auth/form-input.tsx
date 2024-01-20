2a259629a8bc49238b3bddb93227ccc2f50d5f8b
Erick Namukolo <erickmndev@gmail.com>
2024-01-20 12:16:35 +0200

    styling 101

--- src/components/auth/form-input.tsx
+++ src/components/auth/form-input.tsx
@@ -1,21 +1,29 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import TextField from '../../models/field';
+import './form-input.scss';
 
 const FormInput: React.FC<{
   field: TextField;
   onChange: (event: any) => void;
 }> = (props) => {
   return (
-    <Fragment>
-      <label></label>
+    <div className='group'>
+      <label
+        className={`${
+          props.field.input.length ? 'shrink' : null
+        } form-input-label`}
+      >
+        {props.field.display}
+      </label>
       <input
+        className='form-input'
         type={props.field.type}
         required
         onChange={props.onChange}
         name={props.field.display}
         value={props.field.input ?? ''}
       />
-    </Fragment>
+    </div>
   );
 };
 
